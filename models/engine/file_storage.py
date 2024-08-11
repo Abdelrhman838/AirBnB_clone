ebc4d3c09a3bbba0a21b0e53f054526e67f337f9
Mano224 <mano@example.com>
2024-08-11 23:46:37 +0300

    c

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,14 +14,16 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        # self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = "{}.{}".format(obj.__class__.__name__,obj.id)
+        self.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
+            # temp.update(FileStorage.__objects)
+            for key, val in self.__objects.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f, indent=2)
 
